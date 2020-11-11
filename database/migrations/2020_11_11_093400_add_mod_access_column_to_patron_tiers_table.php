f399b6c0d41cd2eda876ef406242bd2caef033ea
Lucraft <lucasbandermann@gmail.com>
2020-11-11 16:38:46 +0100

    Fixed migration

--- database/migrations/2020_11_11_093400_add_mod_access_column_to_patron_tiers_table.php
+++ database/migrations/2020_11_11_093400_add_mod_access_column_to_patron_tiers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddModAccessColumnToPatronTierTable extends Migration
+class AddModAccessColumnToPatronTiersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddModAccessColumnToPatronTierTable extends Migration
      */
     public function up()
     {
-        Schema::table('patron_tier', function (Blueprint $table) {
+        Schema::table('patron_tiers', function (Blueprint $table) {
             $table->boolean('mod_access')->default(false)->nullable();
         });
     }
@@ -25,7 +25,7 @@ class AddModAccessColumnToPatronTierTable extends Migration
      */
     public function down()
     {
-        Schema::table('patron_tier', function (Blueprint $table) {
+        Schema::table('patron_tiers', function (Blueprint $table) {
             $table->dropColumn('mod_access');
         });
     }
