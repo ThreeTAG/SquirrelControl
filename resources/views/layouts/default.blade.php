0d0c87aa2537bc66b2119d096771a301a314b522
Lucraft <lucasbandermann@gmail.com>
2020-11-13 16:45:58 +0100

    Added permission checks in navbar

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,38 +5,55 @@
     <ul class="navbar-nav mr-auto">
         @if(!auth()->guest())
 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
-                   aria-expanded="false">
-                    Administration
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{!! route('users.index') !!}">Users</a>
-                    <a class="dropdown-item" href="{!! route('roles.index') !!}">Roles</a>
-                </div>
-            </li>
+            @if(auth()->user()->checkPermissionTo('users.manage') || auth()->user()->checkPermissionTo('roles.manage'))
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                       aria-haspopup="true"
+                       aria-expanded="false">
+                        Administration
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        @if(auth()->user()->checkPermissionTo('users.manage'))
+                            <a class="dropdown-item" href="{!! route('users.index') !!}">Users</a>
+                        @endif
+                        @if(auth()->user()->checkPermissionTo('roles.manage'))
+                            <a class="dropdown-item" href="{!! route('roles.index') !!}">Roles</a>
+                        @endif
+                    </div>
+                </li>
+            @endif
 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
-                   aria-expanded="false">
-                    Minecraft Players
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{!! route('minecraft-players.index') !!}">Players</a>
-                    <a class="dropdown-item" href="{!! route('patreon.index') !!}">Patreon</a>
-                </div>
-            </li>
+            @if(auth()->user()->checkPermissionTo('minecraft_players.manage') || auth()->user()->checkPermissionTo('patreon.manage'))
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                       aria-haspopup="true"
+                       aria-expanded="false">
+                        Minecraft Players
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        @if(auth()->user()->checkPermissionTo('minecraft_players.manage'))
+                            <a class="dropdown-item" href="{!! route('minecraft-players.index') !!}">Players</a>
+                        @endif
+                        @if(auth()->user()->checkPermissionTo('patreon.manage'))
+                            <a class="dropdown-item" href="{!! route('patreon.index') !!}">Patreon</a>
+                        @endif
+                    </div>
+                </li>
+            @endif
 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
-                   aria-expanded="false">
-                    Accessoires
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{!! route('accessoires.index') !!}">Accessoires</a>
-                    <a class="dropdown-item" href="{!! route('accessoires.sets.index') !!}">Accessoire Sets</a>
-                </div>
-            </li>
+            @if(auth()->user()->checkPermissionTo('accessoires.manage'))
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                       aria-haspopup="true"
+                       aria-expanded="false">
+                        Accessoires
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{!! route('accessoires.index') !!}">Accessoires</a>
+                        <a class="dropdown-item" href="{!! route('accessoires.sets.index') !!}">Accessoire Sets</a>
+                    </div>
+                </li>
+            @endif
 
         @endif
     </ul>
