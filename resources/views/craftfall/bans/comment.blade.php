8f22653af1f8fecf33b33727c6c29c2bbddc4171
Lucas Bandermann <lucasbandermann@gmail.com>
2021-07-17 21:38:23 +0200

    Small layout change for ban comments

--- resources/views/craftfall/bans/comment.blade.php
+++ resources/views/craftfall/bans/comment.blade.php
@@ -2,7 +2,7 @@
      style="width: 70%; margin-bottom: 10px">
     <h4 class="card-header">{!! $comment->commenter->name !!} <i class="fa fa-comment"></i>
         @if($ban->revoked_by_id === $comment->commenter->id && $comment->created_at->eq($ban->revoked_at))
-            <span class="badge badge-success">Revoked Ban</span>
+            <span class="badge badge-success pull-right">Revoked Ban</span>
         @endif
     </h4>
     <div class="card-body">
