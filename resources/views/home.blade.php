e5db4d9b9ea28c8b26c0cc28c0cf15fd3f1556ed
Lucraft <lucasbandermann@gmail.com>
2023-06-15 21:53:58 +0200

    Test change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Hello there!
                 </div>
             </div>
         </div>
