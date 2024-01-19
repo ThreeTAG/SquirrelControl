8ae66db7a2d72d406e9d7a9f877259085d7c2b27
Lucas Bandermann <lbandermann@doegel.de>
2024-01-19 10:48:32 +0100

    Added cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('update:patrons')->dailyAt('05:00');
+        $schedule->command('check:mod-updates')->everyMinute();
     }
 
     /**
